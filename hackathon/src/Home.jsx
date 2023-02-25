a56abc82d97462612f4a0c4d74c1e58b0fb2869b
sanarkk <sanya.dimbrovskyi@gmail.com>
2023-02-25 16:53:48 -0600

    home page

--- hackathon/src/Home.jsx
+++ hackathon/src/Home.jsx
@@ -1,34 +1,12 @@
 import React from "react";
 import {Link} from 'react-router-dom';
 import image from './img/books2.png'
+import NavBar from './components/navBar'
 
 const Home = () => {
   return (
     <div class = "bg-dark" style = {{height:"100vh"}}>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark navbar-dark">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Easycourses</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <Link class="nav-link active" aria-current="page" to="/">Main</Link>
-              </li>
-              <li class="nav-item">
-                <Link class="nav-link" to="/courses">Courses</Link>
-              </li>
-              <li class="nav-item">
-                <Link class="nav-link" to="/profile">Profile</Link>
-              </li>
-              <li class="nav-item">
-                <Link class="nav-link" to="/aboutus">About us</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <NavBar />
 
       <div class = "maindiv">
         <section class = "p-5 text-light text-right">
