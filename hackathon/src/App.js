ce920afee033553c11dfbeff44c4e6ce4eab86a4
harrehmat <harrehmatsinghc@gmail.com>
2023-02-25 15:08:46 -0600

    aboutUs

--- hackathon/src/App.js
+++ hackathon/src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route, Link } from "react-router-dom";
 import Home from "./Home";
 import Login from "./Login";
 import Register from "./Register";
+import AboutUs from "./AboutUs";
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+
       </Routes>
     </div>
   );
