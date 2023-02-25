c1c1e6bd3d832aecb308e93abee580bf652394eb
benedict <agupitab@myumanitoba.ca>
2023-02-25 14:07:14 -0600

    Moved categories to the right

--- hackathon/src/Home.jsx
+++ hackathon/src/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
+            <ul class="navbar-nav ms-auto">
               <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="#">Home</a>
               </li>
